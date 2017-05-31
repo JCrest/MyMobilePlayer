6e2fc94c7c837faaf21814debd1abbc3db3c3003
JCrest <1554687045@qq.com>
2017-05-31 16:13:06 +0800

    修改若干bug后正常运行

--- app/src/main/java/com/example/jiangchuanfa/mymobileplayer/Fragmant/FragmentPage/RecyclerViewFragment.java
+++ app/src/main/java/com/example/jiangchuanfa/mymobileplayer/Fragmant/FragmentPage/RecyclerViewFragment.java
@@ -9,7 +9,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.cjj.MaterialRefreshLayout;
+import com.cjj.MaterialRefreshListener;
 import com.example.jiangchuanfa.mymobileplayer.Adapter.RecyclerFragmentAdapter;
 import com.example.jiangchuanfa.mymobileplayer.Bean.NetAudioBean;
 import com.example.jiangchuanfa.mymobileplayer.Fragmant.BaseFragment;
@@ -25,47 +28,93 @@ import java.util.List;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
+import static android.content.ContentValues.TAG;
+
 
 /**
  * Created by crest on 2017/5/31.
+ *
+ *
  */
 
 public class RecyclerViewFragment extends BaseFragment {
+    //private static final String TAG = RecyclerViewFragment.class.getSimpleName();
     @Bind(R.id.recyclerview)
     RecyclerView recyclerview;
+    @Bind(R.id.refresh)
+    MaterialRefreshLayout refresh;
     @Bind(R.id.progressbar)
     ProgressBar progressbar;
     @Bind(R.id.tv_nomedia)
     TextView tvNomedia;
+//    @Bind(R.id.)
+//    ListView listview;
+//    @Bind(R.id.progressbar)
+//    ProgressBar progressbar;
+//    @Bind(R.id.tv_nomedia)
+//    TextView tvNomedia;
 
     private List<NetAudioBean.ListBean> listDatas;
     private RecyclerFragmentAdapter myAdapter;
+    private MaterialRefreshLayout materialRefreshLayout;
+    private boolean isLoadMore = false;
 
     @Override
     public View initView() {
-        Log.e("TAG", "网络音频ui初始化了。。");
+        Log.e(TAG, "网络音频UI被初始化了");
         View view = View.inflate(mContext, R.layout.fragment_recyclerview, null);
-        ButterKnife.bind(this,view);
+        ButterKnife.bind(this, view);
+        materialRefreshLayout = (MaterialRefreshLayout) view.findViewById(R.id.refresh);
+        materialRefreshLayout.setMaterialRefreshListener(new MaterialRefreshListener() {
+            @Override
+            public void onRefresh(MaterialRefreshLayout materialRefreshLayout) {
+                isLoadMore = false;
+                getDataFromNet();
+            }
 
+            @Override
+            public void onRefreshLoadMore(MaterialRefreshLayout materialRefreshLayout) {
+                super.onRefreshLoadMore(materialRefreshLayout);
+                isLoadMore = true;
+                getMoreData();
+            }
+        });
 
+//        recyclerview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//
+//
+//                NetAudioInfo.ListBean listEntity = listDatas.get(position);
+//                if (listEntity != null) {
+//                    //3.传递视频列表
+//                    Intent intent = new Intent(context, PicassoSampleActivity.class);
+//                    if (listEntity.getType().equals("gif")) {
+//                        String url = listEntity.getGif().getImages().get(0);
+//                        intent.putExtra("url", url);
+//                        context.startActivity(intent);
+//                    } else if (listEntity.getType().equals("image")) {
+//                        String url = listEntity.getImage().getThumbnail_small().get(0);
+//                        intent.putExtra("url", url);
+//                        context.startActivity(intent);
+//                    }
+//                }
+//
+//
+//            }
+//        });
         return view;
     }
 
-    @Override
-    public void initData() {
-        super.initData();
-        Log.e("TAG", "网络音频数据初始化了。。");
-        getDataFromNet();
-    }
-
-
-    private void getDataFromNet() {
+    private void getMoreData() {
         RequestParams params = new RequestParams("http://s.budejie.com/topic/list/jingxuan/1/budejie-android-6.2.8/0-20.json?market=baidu&udid=863425026599592&appname=baisibudejie&os=4.2.2&client=android&visiting=&mac=98%3A6c%3Af5%3A4b%3A72%3A6d&ver=6.2.8");
         x.http().get(params, new Callback.CommonCallback<String>() {
             @Override
             public void onSuccess(String result) {
+
                 Log.e("TAG", "网络音乐请求成功" + result);
                 processData(result);
+                materialRefreshLayout.finishRefreshLoadMore();
             }
 
             @Override
@@ -83,31 +132,78 @@ public class RecyclerViewFragment extends BaseFragment {
 
             }
         });
+
+
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+        Log.e(TAG, "网络音频数据初始化了");
+        getDataFromNet();
+    }
+
+    private void getDataFromNet() {
+        RequestParams params = new RequestParams("http://s.budejie.com/topic/list/jingxuan/1/budejie-android-6.2.8/0-20.json?market=baidu&udid=863425026599592&appname=baisibudejie&os=4.2.2&client=android&visiting=&mac=98%3A6c%3Af5%3A4b%3A72%3A6d&ver=6.2.8");
+        x.http().get(params, new Callback.CommonCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+
+                Log.e("TAG", "网络音乐请求成功" + result);
+                processData(result);
+                Toast.makeText(mContext, "刷新成功", Toast.LENGTH_SHORT).show();
+                materialRefreshLayout.finishRefresh();
+            }
+
+            @Override
+            public void onError(Throwable ex, boolean isOnCallback) {
+                Log.e("TAG", "网络音乐请求失败" + ex.getMessage());
+            }
+
+            @Override
+            public void onCancelled(CancelledException cex) {
+
+            }
+
+            @Override
+            public void onFinished() {
+
+            }
+        });
+
+
     }
 
     private void processData(String json) {
         NetAudioBean netAudioBean = new Gson().fromJson(json, NetAudioBean.class);
-        listDatas = netAudioBean.getList();
 
 
-        Log.e("TAG", "解决成功==" + listDatas.get(0).getText());
-        if (listDatas != null && listDatas.size() > 0) {
-            //有视频
-            tvNomedia.setVisibility(View.GONE);
-            //设置适配器
-            myAdapter = new RecyclerFragmentAdapter(mContext, listDatas);
-            recyclerview.setAdapter(myAdapter);
+        if (!isLoadMore) {
+            listDatas = netAudioBean.getList();
+            Log.e("TAG", "解决成功==" + listDatas.get(0).getText());
+            if (listDatas != null && listDatas.size() > 0) {
+                //有视频
+                tvNomedia.setVisibility(View.GONE);
+                //设置适配器
+                myAdapter = new RecyclerFragmentAdapter(mContext, listDatas);
+                recyclerview.setAdapter(myAdapter);
+                recyclerview.setLayoutManager(new LinearLayoutManager(mContext,LinearLayoutManager.VERTICAL,false));
+            } else {
+                //没有视频
+                tvNomedia.setVisibility(View.VISIBLE);
+            }
 
-            recyclerview.setLayoutManager(new LinearLayoutManager(mContext, LinearLayoutManager.VERTICAL, false));
+
         } else {
-            //没有视频
-            tvNomedia.setVisibility(View.VISIBLE);
+            List<NetAudioBean.ListBean> NewList = netAudioBean.getList();
+            listDatas.addAll(NewList);
+            myAdapter.notifyDataSetChanged();
         }
 
         progressbar.setVisibility(View.GONE);
 
-    }
 
+    }
 
     @Override
     public void onDestroyView() {
